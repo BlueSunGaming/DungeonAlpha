7d388b39a9daf32d149edd504760477fbc9a0c45
blue sun <chadsp@gmail.com>
2018-06-15 22:30:12 -0500

    Prepare for a new BuildingUpgrade node for custom function

---
+++ Assets/RPGAIO/Scripts/RPGMaker/Nodes/Core/BuildingUpgrade.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+
+namespace LogicSpawn.RPGMaker.Core
+{
+    [NodeCategory("GameObject", "")]
+    public class BuildingUpgrade : SimpleNode
+    {
+        public override string Name
+        {
+            get { return "Upgrade a Building"; }
+        }
+
+        public override string Description
+        {
+            get { return "Upgrade a Building if possible"; }
+        }
+
+        public override string SubText
+        {
+            get { return ""; }
+        }
+
+        public override bool CanBeLinkedTo
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override string NextNodeLinkLabel(int index)
+        {
+            return "Next";
+        }
+
+        protected override void SetupParameters()
+        {
+            Add("GameObject", PropertyType.GameObject, null, "", PropertySource.EnteredOrInput, PropertyFamily.Object);
+            Add("Destroy After", PropertyType.Float, null, 0);
+        }
+
+        protected override void Eval(NodeChain nodeChain)
+        {
+            GameObject gameObject = (GameObject)ValueOf("GameObject");
+            var destroyAfter = (float)ValueOf("Destroy After");
+
+            if (gameObject != null)
+            {
+                Object.Destroy(gameObject, destroyAfter);
+            }
+        }
+    }
+}
