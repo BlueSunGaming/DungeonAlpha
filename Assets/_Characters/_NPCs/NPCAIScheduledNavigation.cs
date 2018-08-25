f2aa008f11c84386564f7c47b0e743ccc7796586
blue sun <chadsp@gmail.com>
2018-08-25 17:10:08 -0500

    Add new class to move a npc to a set Destination within scene that is predefined using Tag "Destination"

---
+++ Assets/_Characters/_NPCs/NPCAIScheduledNavigation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LogicSpawn.RPGMaker.Core;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class NPCAIScheduledNavigation : MonoBehaviour {
+
+    GameObject targetDestination = null;
+    public float speed = 1;
+
+    // Use this for initialization
+    void Start () {
+        // Move to targeted point
+        targetDestination = GameObject.FindGameObjectWithTag("Destination");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float move = speed * Time.deltaTime;	   	       	        
+        transform.position = Vector3.MoveTowards(transform.position, targetDestination.transform.position, move);
+	}
+
+
+}
+
