79a07c29796fe4a8e93c56b8c233278894d0ab9f
blue sun <chadsp@gmail.com>
2018-08-25 18:21:29 -0500

    updates to make scheduled navigation possible

--- Assets/_Characters/_NPCs/NPCAIScheduledNavigation.cs
+++ Assets/_Characters/_NPCs/NPCAIScheduledNavigation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using LogicSpawn.RPGMaker.Core;
 using UnityEngine;
@@ -7,22 +8,90 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class NPCAIScheduledNavigation : MonoBehaviour {
 
-    GameObject targetDestination = null;
-    public float speed = 1;
+    //GameObject targetDestination = null;
+    
+    public List<GameObject> targetDestinations;
+    private int currentIndex_Iter=0;
+    public float speed = 2;
+    //private bool isMoving = false;
+    //private bool isWaiting = false;
 
     // Use this for initialization
     void Start () {
-        // Move to targeted point
-        targetDestination = GameObject.FindGameObjectWithTag("Destination");
+        // Move to targeted point- Initial condition
+        MoveToDestination(4.0f);
     }
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	    float move = speed * Time.deltaTime;	   	       	        
-        transform.position = Vector3.MoveTowards(transform.position, targetDestination.transform.position, move);
+	    float move = speed * Time.deltaTime;
+
+        GameObject currentGameObject = GetCurrentGameObject();
+	    if (currentGameObject != null)
+	    {
+	        if (transform.position == currentGameObject.transform.position)
+	        {
+	            currentIndex_Iter++;
+	            StartCoroutine(WaitThenDo(MoveToDestination));
+	        }
+	    }
+	    else
+	    {
+	        currentIndex_Iter = 0;
+	    }
+
 	}
 
+    private IEnumerator WaitThenDo(object v)
+    {
+        throw new NotImplementedException();
+    }
+
+    IEnumerator WaitThenDo(System.Action thingToDo)
+    {
+        yield return new WaitForSeconds(5);
+        thingToDo();
+    }
+
+    void MoveToDestination()
+    {
+        float move = speed * Time.deltaTime;
+        GameObject currentGameObject = GetCurrentGameObject();
+        if (currentGameObject != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentGameObject.transform.position, move);
+        }
+
+    }
+
+    void MoveToDestination(float timeSinceLastFrame)
+    {
+        float move = speed * timeSinceLastFrame;
+        GameObject currentGameObject = GetCurrentGameObject();
+        if (currentGameObject != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentGameObject.transform.position, move);
+        }
+        
+    }
+
+
+    GameObject GetCurrentGameObject()
+    {
+        GameObject returnGameObject = null;
+        if (currentIndex_Iter == targetDestinations.Count)
+        {
+            currentIndex_Iter = 0;
+        }
+
+        if (currentIndex_Iter < targetDestinations.Count && currentIndex_Iter > -1)
+        {
+            returnGameObject = targetDestinations[currentIndex_Iter];
+        }
+
+        return returnGameObject;
+    }
 
 }
 
